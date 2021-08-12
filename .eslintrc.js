53f6b32b5bda6bc9594827aba4db0cea12dd36da
liupingfu <liupingfu@tal.com>
2021-08-12 17:05:46 +0800

    fix: 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,64 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    root: true,
+    env: {
+      node: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
+    'extends': [
+      'plugin:vue/strongly-recommended',
+      '@vue/standard'
     ],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    rules: {
+      'no-console': 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'generator-star-spacing': 'off',
+      'no-mixed-operators': 0,
+      'vue/max-attributes-per-line': [
+        2,
+        {
+          'singleline': 5,
+          'multiline': {
+            'max': 1,
+            'allowFirstLine': false
+          }
+        }
+      ],
+      'vue/attribute-hyphenation': 0,
+      'vue/html-self-closing': 0,
+      'vue/component-name-in-template-casing': 0,
+      'vue/html-closing-bracket-spacing': 0,
+      'vue/singleline-html-element-content-newline': 0,
+      'vue/no-unused-components': 0,
+      'vue/multiline-html-element-content-newline': 0,
+      'vue/no-use-v-if-with-v-for': 0,
+      'vue/html-closing-bracket-newline': 0,
+      'vue/no-parsing-error': 0,
+      'no-tabs': 0,
+      'quotes': [
+        2,
+        'single',
+        {
+          'avoidEscape': true,
+          'allowTemplateLiterals': true
+        }
+      ],
+      'semi': [
+        2,
+        'never',
+        {
+          'beforeStatementContinuationChars': 'never'
+        }
+      ],
+      'no-delete-var': 2,
+      'prefer-const': [
+        2,
+        {
+          'ignoreReadBeforeAssign': false
+        }
+      ],
+      'template-curly-spacing': 'off',
+      'indent': 'off'
     },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-        "semi": ["error", "never"],
-        'no-multi-spaces': ['error', { "ignoreEOLComments": true }]
+    parserOptions: {
+      parser: 'babel-eslint'
     }
-};
+  }
--- commitlint.config.js
+++ commitlint.config.js
@@ -12,10 +12,15 @@ module.exports = {
         'refactor', // 重构
         'test', // 测试
         'revert', // 回滚
-        'chore' // 构建过程或辅助工具的变动
+        'chore' // 其他
       ]
     ],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
     'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never']
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72]
   }
 }
