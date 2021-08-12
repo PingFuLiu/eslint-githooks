74dbd4830b588b832c76b1916e129715646ba3a6
liupingfu <liupingfu@tal.com>
2021-08-12 17:07:21 +0800

    fix: 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -60,5 +60,14 @@ module.exports = {
     },
     parserOptions: {
       parser: 'babel-eslint'
-    }
+    },
+    overrides: [{
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }]
   }
